b525b21ed01ab46d5edc64c624dd5c7fdf5a572a
jerry6290 <jerry6290@163.com>
2014-01-08 15:48:11 +0800

    //if roll period,roll dirctory by date format:'yyyy-MM-dd'

--- src/store.cpp
+++ src/store.cpp
@@ -442,14 +442,15 @@ string FileStoreBase::makeFullSymlink() {
 
 string FileStoreBase::makeBaseFilename(struct tm* creation_time) {
   ostringstream filename;
-
-  filename << baseFileName;
+  
   if (rollPeriod != ROLL_NEVER) {
-    filename << '-' << creation_time->tm_year + 1900  << '-'
+    //if roll period,roll dictory by date format:'yyyy-MM-dd'
+    filename << creation_time->tm_year + 1900  << '-'
              << setw(2) << setfill('0') << creation_time->tm_mon + 1 << '-'
-             << setw(2) << setfill('0')  << creation_time->tm_mday;
+             << setw(2) << setfill('0')  << creation_time->tm_mday << '/';
 
   }
+  filename  << baseFileName;
   return filename.str();
 }
 
